a71f21c77787ecb792b4bcead2f73c4dfc906aa3
svigezzi <56412122+svigezzi@users.noreply.github.com>
2026-01-14 10:20:53 +0100

    Add files via upload

---
+++ Day 3/Exercise3_empty.R
@@ -0,0 +1,27 @@
+# Setup -------------------------------------------------------------------
+
+library(here)
+wd <- here()
+setwd(wd)
+
+load("Day 3/Exercise 3/AburtoBeltranSanchez.RData")
+
+library(tidyverse)
+library(DemoDecomp)
+
+source("Day 3/Exercise 3/Functions_D3E3.R")
+
+# Linear integral method --------------------------------------------------
+
+# Let's have a look at the decomposition function
+horiuchi
+
+# Let's have a look at the functions
+edagger.frommx
+
+# there are also other functions
+sd.frommx
+rG.frommx
+
+
+
---
+++ Day 3/Exercise4_empty.R
@@ -0,0 +1,19 @@
+# Setup -------------------------------------------------------------------
+
+library(here)
+wd <- here()
+setwd(wd)
+
+load("Day 3/Exercise 3/AburtoBeltranSanchez.RData")
+
+library(tidyverse)
+library(DemoDecomp)
+
+source("Day 3/Exercise 4/Functions_D3E4.R")
+
+
+# Linear integral method --------------------------------------------------
+
+# Let's have a look at the lifespan variation function
+edagger.frommxc
+
